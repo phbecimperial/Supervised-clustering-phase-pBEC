e05d54ab9f0174cf0621ec7049ed9e9707ea36d3
phbecimperial <photobec@imperial.ac.uk>
2024-01-30 15:23:47 +0000

    Clustering successful

--- MultiMode Analysis/Clustering.py
+++ MultiMode Analysis/Clustering.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import torch
 import numpy as np
 from sklearn.cluster import KMeans
@@ -5,7 +6,8 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 
-batch_size = 64
+
+#batch_size = 64
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = torch.load("cnn.pt")
@@ -13,7 +15,7 @@ newmodel = torch.nn.Sequential(*(list(model.children())[:-2]))
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (
     0.5,))])  # Transforms it to a tensor, and rescales pixel values [0, 1]
 test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(dataset=test_dataset, batch_size=1, shuffle=False)
 
 
 
@@ -25,13 +27,42 @@ with torch.no_grad():
     for inputs, labels in test_loader:
         inputs = inputs.to(device)
 
-        outputs = model(inputs)
-        all_features.append(outputs.cpu().numpy())
+        outputs = newmodel(inputs)
+        outputs = outputs.cpu().numpy()
+        outputs = outputs[0].tolist()
+        all_features.append(outputs)
 
 all_features = np.array(all_features)
 
 #Insert PCA if needed
 target_names=test_dataset.classes
-kmeans = KMeans(n_clusters = len(target_names), n_jobs=-1)
+kmeans = KMeans(n_clusters = len(target_names))
 kmeans.fit(all_features)
+
+#Figure out which pieces of data are at what point, labelled by the indices.
+groups = {}
+for i in range(0, len(kmeans.labels_)):
+    cluster = kmeans.labels_[i]
+    if kmeans.labels_[i] not in groups.keys():
+        groups[cluster] = []
+        groups[cluster].append(i)
+    else:
+        groups[cluster].append(i)
+#%%
+#View clusters
+def view_cluster(cluster):
+    indices = groups[cluster]
+    if len(indices) > 30:
+        indices = indices[:29]
+
+    plt.figure(figsize=[25,25])
+
+    for i in range(0, len(indices)):
+        plt.subplot(10, 10, i+1)
+        img = test_dataset[indices[i]][0].cpu().numpy()
+        img = img[0]
+        plt.imshow(img)
+
+view_cluster(2)
+plt.show()
 
--- MultiMode Analysis/neural_net.py
+++ MultiMode Analysis/neural_net.py
@@ -4,15 +4,15 @@ import torch.nn as nn
 from torch.nn import Module as M
 from tqdm import tqdm as tqdm
 
-
 batch_size = 1
+
+
 class CNN(nn.Module):
     def __init__(self, classes, layer1=32, layer2=64, layer_k=3, pool_k=2):
         super().__init__()
         self.conv1 = nn.Conv2d(1, layer1, layer_k, stride=1, padding=1)
         self.relu = nn.ReLU()
         self.maxpool1 = nn.MaxPool2d(pool_k, stride=2)
-
 
         self.conv2 = nn.Conv2d(layer1, layer2, layer_k, stride=1, padding=1)
         self.relu2 = nn.ReLU()
@@ -24,40 +24,6 @@ class CNN(nn.Module):
         self.relu3 = nn.ReLU()
 
         self.fc4 = nn.Linear(128, classes)
-
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.maxpool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
-        x = self.flat(x)
-        x = self.fc3(x)
-        x = self.relu3(x)
-        x = self.fc4(x)
-        return x
-
-class CNN(nn.Module):
-    def __init__(self, classes, layer1=32, layer2=64, layer_k=3, pool_k=2):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, layer1, layer_k, stride=1, padding=1)
-        self.relu = nn.ReLU()
-        self.maxpool1 = nn.MaxPool2d(pool_k, stride=2)
-
-
-        self.conv2 = nn.Conv2d(layer1, layer2, layer_k, stride=1, padding=1)
-        self.relu2 = nn.ReLU()
-        self.maxpool2 = nn.MaxPool2d(pool_k, stride=2, padding=1)
-
-        self.flat = nn.Flatten()
-
-        self.fc3 = nn.LazyLinear(128)
-        self.relu3 = nn.ReLU()
-
-        self.fc4 = nn.Linear(128, classes)
-
 
     def forward(self, x):
         x = self.conv1(x)
@@ -73,7 +39,33 @@ class CNN(nn.Module):
         return x
 
 
+class CNN(nn.Module):
+    def __init__(self, classes, layer1=32, layer2=64, layer_k=3, pool_k=2):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, layer1, layer_k, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(pool_k, stride=2)
 
+        self.conv2 = nn.Conv2d(layer1, layer2, layer_k, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(pool_k, stride=2, padding=1)
 
+        self.flat = nn.Flatten()
 
+        self.fc3 = nn.LazyLinear(128)
+        self.relu3 = nn.ReLU()
 
+        self.fc4 = nn.Linear(128, classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+        x = self.flat(x)
+        x = self.fc3(x)
+        x = self.relu3(x)
+        x = self.fc4(x)
+        return x
