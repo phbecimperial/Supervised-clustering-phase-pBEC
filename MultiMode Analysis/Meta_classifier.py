ecace0f79d32a17274a35870c92d6761bedb74a4
phbecimperial <photobec@imperial.ac.uk>
2024-02-21 00:26:42 +0000

    Upgrade

--- MultiMode Analysis/Meta_classifier.py
+++ MultiMode Analysis/Meta_classifier.py
@@ -5,23 +5,21 @@ from Fcmeans_utils import CustomModel
 from torch.utils.data import DataLoader
 from pickle_Dataset import CustomDataset
 from tqdm import tqdm
+from sklearn.metrics.pairwise import euclidean_distances
 
 
 
 # Load centroids and membership values from file
 
+def predict(data, cluster_centres, m):
+        D = 1.0 / euclidean_distances(data, cluster_centres, squared=True)
+        D **= 1.0 / (m - 1)
+        D /= np.sum(D, axis=1)[:, np.newaxis]
+
+        return D
 
 
-def predict_clusters(new_data, i):
-    with open('fuzzy_kmeans_clusters.pkl' + str(i), 'rb') as f:
-        centroids, fuzzmatrix, cluster_to_class = pickle.load(f)
-    distances = np.linalg.norm(new_data[:, np.newaxis, :] - centroids, axis=2)
 
-    # Assign each new data point to the cluster with the highest membership value
-    predicted_labels = np.argmax(fuzzmatrix, axis=1)
-
-    print(cluster_to_class)
-    return predicted_labels
 
 
 #Get the new data
@@ -59,11 +57,17 @@ for i in range(0, 10):
 
     all_features = np.array(all_features)
 
+    with open('fuzzy_kmeans_clusters.pkl' + str(i), 'rb') as f:
+        cluster_centres, fuzzmatrix, cluster_to_class = pickle.load(f)
+
+    print(cluster_to_class)
+
+
+
 
 
     # Predict the clusters of new data points
-    predicted_clusters = predict_clusters(all_features, i)
-
+    predicted_clusters = predict(all_features, cluster_centres)
     print("Predicted clusters for new data points:")
     print(predicted_clusters)
 
