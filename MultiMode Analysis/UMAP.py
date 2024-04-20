eb5ff3b007637597d4b34c9db8bd0c833f09542d
Dak <natakala.d.n@gmail.com>
2024-04-20 23:36:24 +0100

    umap update

--- MultiMode Analysis/UMAP.py
+++ MultiMode Analysis/UMAP.py
@@ -22,6 +22,9 @@ def read(bec_file: str, files: list[str]):
 
     all_info = [file.split('_') for file in files]
     int_times = [float(file[4]) for file in all_info]
+    pwrs = [float(file[5]) for file in all_info]
+    wavelengths = [float(file[6]) for file in all_info]
+
     bec_im = cv2.imread(bec_file, 0)
     cx, cy = center_of_mass(bec_im ** 3)
 
@@ -36,7 +39,7 @@ def read(bec_file: str, files: list[str]):
                      int(int(cy) - size[1] / 2):int(int(cy) + size[1] / 2)]
 
         image = image.flatten()
-        image = image/int_times[i]
+        #image = image/int_times[i]
 
 
 
@@ -44,17 +47,32 @@ def read(bec_file: str, files: list[str]):
 
     img_features = np.array(img_features)
 
-    return img_features
+    return img_features, np.array(pwrs), np.array(wavelengths)
 
 plt.style.use(['science','ieee'])
 
+becpath = r'C:\Users\natak\OneDrive - Imperial College London\Documents\University\Year 4\MSci Project-r018104\AprData\Final_data\pbec_20240321_003143_62499.0_0.18152103448275864_940.0557861328125_9.0_.png'
+path=r'C:\Users\natak\OneDrive - Imperial College London\Documents\University\Year 4\MSci Project-r018104\AprData\Final_data'
+files = glob.glob(path+'\*.png')
 
-def umap2d(neighbors, min_dist):
+img_features, pwrs, wavelengths = read(becpath, files)
+
+#Filtering
+mask = np.max(img_features, axis=1) >= 50
+img_features = img_features[mask]
+pwrs = pwrs[mask]
+wavelengths = wavelengths[mask]
+
+pca = PCA(n_components=100, random_state=22)
+pca.fit(img_features)
+img_features = pca.transform(img_features)
+#%%
+def umap2d(neighbors, min_dist, embedding_dim=2, kmeans_clusters=5):
     # Initialize UMAP
     reducer = umap.UMAP(n_neighbors=neighbors, min_dist=min_dist, random_state=22)
     embedding = reducer.fit_transform(img_features)
 
-    kmeans = KMeans(n_clusters=10, random_state=22)
+    kmeans = KMeans(n_clusters=kmeans_clusters, random_state=22)
     kmeans.fit(img_features)
     
     # Plot
@@ -64,19 +82,15 @@ def umap2d(neighbors, min_dist):
     plt.ylabel('UMAP 2')
     plt.show()
 
-becpath = r'C:\Users\natak\OneDrive - Imperial College London\Documents\University\Year 4\MSci Project-r018104\AprData\Final_data\pbec_20240321_003143_62499.0_0.18152103448275864_940.0557861328125_9.0_.png'
-path=r'C:\Users\natak\OneDrive - Imperial College London\Documents\University\Year 4\MSci Project-r018104\AprData\Final_data'
-files = glob.glob(path+'\*.png')
+    reducer = umap.UMAP(n_neighbors=neighbors, min_dist=min_dist,
+                        n_components=embedding_dim, random_state=22)
+    embedding = reducer.fit_transform(img_features)
 
-img_features = read(becpath, files)
+    lab = embedding, kmeans.labels_
+    return lab
 
-#Filtering
-mask = np.max(img_features, axis=1) >= 50/500000
-img_features = img_features[mask]
 
-pca = PCA(n_components=100, random_state=22)
-pca.fit(img_features)
-img_features = pca.transform(img_features)
+
 
 
 # #%%
@@ -99,13 +113,28 @@ img_features = pca.transform(img_features)
 
 if __name__ == '__main__':
     #Distances
-    min_dists = np.linspace(0, 1, 11)
-    for d in tqdm.tqdm(min_dists):
-        umap2d(14, d)
-        gc.collect()
+    # min_dists = np.linspace(0, 1, 11)
+    # for d in tqdm.tqdm(min_dists):
+    #     umap2d(14, d)
+    #     gc.collect()
+
+    embedding, labels = umap2d(10, 0.2, embedding_dim=2, kmeans_clusters=5)
+
+    plt.scatter(wavelengths, pwrs, c=labels)
+    plt.title('K-means, no umap')
+    plt.show()
+
+    kmeans = KMeans(n_clusters=3, random_state=22)
+    kmeans.fit(embedding)
+
+    plt.scatter(wavelengths, pwrs, c=kmeans.labels_)
+    plt.show()
+
+    plt.scatter(embedding[:, 0], embedding[:, 1], s=5, c=kmeans.labels_, cmap='viridis')
+    plt.title('K-means, umap')
+    plt.show()
 
 
-#%%
 # reducer = umap.UMAP(n_components=3)
 # embedding = reducer.fit_transform(img_features)
 
@@ -120,8 +149,6 @@ if __name__ == '__main__':
 #%%
 
 
-# neigh = np.linspace(4, 50, 5, dtype=int)
-# min_dists = np.linspace(0, 1, 5)
-# for n in tqdm.tqdm(neigh):
-#     for d in min_dists:
-#         umap2d(n, d)
+
+
+
